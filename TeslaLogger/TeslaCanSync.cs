bc30dba25fb0f93d1300ac163a0acfbbbdad3cac
Martin <martin@gerczuk.de>
2020-07-10 19:53:32 +0200

    Refactored TeslaCAN Sync

--- TeslaLogger/TeslaCanSync.cs
+++ TeslaLogger/TeslaCanSync.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Net.Http;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Web.Script.Serialization;
 using MySql.Data.MySqlClient;
@@ -10,194 +12,185 @@ namespace TeslaLogger
 {
     public class TeslaCanSync
     {
-        private string token;
-        private System.Threading.Thread thread;
-        private bool run = true;
+        private const int SECONDS = 10;
+
+        private readonly bool run = true;
+        private readonly Thread thread;
+        private readonly string token;
 
         public TeslaCanSync(string token)
         {
             this.token = token;
 
-            thread = new System.Threading.Thread(new System.Threading.ThreadStart(Start));
+            thread = new Thread(Start);
             thread.Start();
         }
 
         private void Start()
         {
-            //if (!Tools.UseScanMyTesla())
-            //{
-            //    return;
-            //}
-
-            Logfile.Log("Start TeslaCAN Thread!");
-
-            string response = "";
+            Logfile.Log("Start refactored TeslaCAN Thread!");
 
             while (run)
             {
                 try
                 {
-                    System.Threading.Thread.Sleep(500);
+                    var data = GetTeslaCanData().Result;
 
-                    response = GetDataFromWebservice().Result;
-                    if (response.StartsWith("not found") || response.StartsWith("ERROR:"))
+                    if (data == null)
                     {
-                        System.Threading.Thread.Sleep(5000);
+                        // car sleeping...
+                        Thread.Sleep(20000);
                     }
                     else
                     {
-                        InsertData(response);
+                        SaveData(data);
+
+                        var lag = DateTime.Now - data.Timestamp;
+                        if (lag < TimeSpan.FromSeconds(SECONDS))
+                            Thread.Sleep((int) (SECONDS - lag.TotalSeconds + 0.5) * 1000);
                     }
                 }
                 catch (Exception ex)
                 {
                     Logfile.Log("TeslaCAN: " + ex.Message);
                     Logfile.WriteException(ex.ToString());
-                    System.Threading.Thread.Sleep(20000);
+                    Thread.Sleep(20000);
                 }
             }
         }
 
-        private void InsertData(string response)
+        private async Task<Data> GetTeslaCanData()
         {
-            //Logfile.Log("ScanMyTesla: " + response);
+            HttpClient client = new HttpClient();
+            FormUrlEncodedContent content = new FormUrlEncodedContent(new[]
+            {
+                new KeyValuePair<string, string>("t", token)
+            });
+
+            DateTime start = DateTime.UtcNow;
+            HttpResponseMessage result =
+                await client.PostAsync("http://teslacan.fritz.box:8080/get_scanmytesla", content);
+            var resultContent = await result.Content.ReadAsStringAsync();
+
+            DBHelper.AddMothershipDataToDB("teslacan.fritz.box:8080/get_scanmytesla", start, (int) result.StatusCode);
+
+            if (resultContent == "not found") return null;
+
+            string temp = resultContent;
+            var i = temp.IndexOf("\r\n");
+            string id = temp.Substring(0, i);
+
+            temp = temp.Substring(i + 2);
+
+            i = temp.IndexOf("\r\n");
+            string date = temp.Substring(0, i);
+            temp = temp.Substring(i + 2);
+
+            dynamic j = new JavaScriptSerializer().DeserializeObject(temp);
+
+            var data = new Data
+            {
+                Timestamp = DateTime.Parse(j["d"]),
+                Values = (Dictionary<string, object>) j["dict"]
+            };
+
+            return data;
         }
 
-        public async Task<string> GetDataFromWebservice()
+        private static void SaveData(Data data)
         {
-            string resultContent = "";
-            try
+            DBHelper.currentJSON.lastScanMyTeslaReceived = data.Timestamp;
+            DBHelper.currentJSON.CreateCurrentJSON();
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append("INSERT INTO `can` (`datum`, `id`, `val`) VALUES ");
+            bool first = true;
+
+            string sqlDate = data.Timestamp.ToString("yyyy-MM-dd HH:mm:ss");
+
+            foreach (KeyValuePair<string, object> line in data.Values)
             {
-                HttpClient client = new HttpClient();
-                FormUrlEncodedContent content = new FormUrlEncodedContent(new[]
+                if (line.Value.ToString().Contains("Infinity") || line.Value.ToString().Contains("NaN"))
                 {
-                    new KeyValuePair<string, string>("t", token)
-                });
-
-                DateTime start = DateTime.UtcNow;
-                HttpResponseMessage result = await client.PostAsync("http://teslacan.fritz.box:8080/get_scanmytesla", content);
-                resultContent = await result.Content.ReadAsStringAsync();
-
-                DBHelper.AddMothershipDataToDB("teslacan.fritz.box:8080/get_scanmytesla", start, (int)result.StatusCode);
-
-                if (resultContent == "not found")
-                {
-                    return "not found";
+                    continue;
                 }
 
-                string temp = resultContent;
-                int i = 0;
-                i = temp.IndexOf("\r\n");
-                string id = temp.Substring(0, i);
-
-                temp = temp.Substring(i+2);
-
-                i = temp.IndexOf("\r\n");
-                string date = temp.Substring(0, i);
-                temp = temp.Substring(i + 2);
-
-                dynamic j = new JavaScriptSerializer().DeserializeObject(temp);
-                DateTime d = DateTime.Parse(j["d"]);
-                DBHelper.currentJSON.lastScanMyTeslaReceived = d;
-                DBHelper.currentJSON.CreateCurrentJSON();
-
-                Dictionary<string, object> kv = (Dictionary<string, object>)j["dict"];
-
-                StringBuilder sb = new StringBuilder();
-                sb.Append("INSERT INTO `can` (`datum`, `id`, `val`) VALUES ");
-                bool first = true;
-
-                string sqlDate =  d.ToString("yyyy-MM-dd HH:mm:ss");
-
-                foreach (KeyValuePair<string, object> line in kv)
+                switch (line.Key)
                 {
-                    if (line.Value.ToString().Contains("Infinity") || line.Value.ToString().Contains("NaN"))
-                    {
-                        continue;
-                    }
+                    case "2":
+                        DBHelper.currentJSON.SMTCellTempAvg = Convert.ToDouble(line.Value);
+                        break;
+                    case "5":
+                        DBHelper.currentJSON.SMTCellMinV = Convert.ToDouble(line.Value);
+                        break;
+                    case "6":
+                        DBHelper.currentJSON.SMTCellAvgV = Convert.ToDouble(line.Value);
+                        break;
+                    case "7":
+                        DBHelper.currentJSON.SMTCellMaxV = Convert.ToDouble(line.Value);
+                        break;
+                    case "28":
+                        DBHelper.currentJSON.SMTBMSmaxCharge = Convert.ToDouble(line.Value);
+                        break;
+                    case "29":
+                        DBHelper.currentJSON.SMTBMSmaxDischarge = Convert.ToDouble(line.Value);
+                        break;
+                    case "442":
+                        if (Convert.ToDouble(line.Value) == 287.6) // SNA - Signal not Available
+                        {
+                            DBHelper.currentJSON.SMTSpeed = 0;
+                            Logfile.Log("SMT Speed: Signal not Available");
+                        }
+                        else
+                        {
+                            DBHelper.currentJSON.SMTSpeed = Convert.ToDouble(line.Value);
+                        }
 
-                    switch (line.Key)
-                    {
-                        case "2":
-                            DBHelper.currentJSON.SMTCellTempAvg = Convert.ToDouble(line.Value);
-                            break;
-                        case "5":
-                            DBHelper.currentJSON.SMTCellMinV = Convert.ToDouble(line.Value);
-                            break;
-                        case "6":
-                            DBHelper.currentJSON.SMTCellAvgV = Convert.ToDouble(line.Value);
-                            break;
-                        case "7":
-                            DBHelper.currentJSON.SMTCellMaxV = Convert.ToDouble(line.Value);
-                            break;
-                        case "28":
-                            DBHelper.currentJSON.SMTBMSmaxCharge = Convert.ToDouble(line.Value);
-                            break;
-                        case "29":
-                            DBHelper.currentJSON.SMTBMSmaxDischarge = Convert.ToDouble(line.Value);
-                            break;
-                        case "442":
-                            if (Convert.ToDouble(line.Value) == 287.6) // SNA - Signal not Available
-                            {
-                                DBHelper.currentJSON.SMTSpeed = 0;
-                                Logfile.Log("SMT Speed: Signal not Available");
-                            }
-                            else
-                            {
-                                DBHelper.currentJSON.SMTSpeed = Convert.ToDouble(line.Value); 
-                            }
-                            break;
-                        case "43":
-                            DBHelper.currentJSON.SMTBatteryPower = Convert.ToDouble(line.Value);
-                            break;
-                        default:
-                            break;
-                    }
+                        break;
+                    case "43":
+                        DBHelper.currentJSON.SMTBatteryPower = Convert.ToDouble(line.Value);
+                        break;
+                }
 
 
-                    if (first)
-                    {
-                        first = false;
-                    }
-                    else
-                    {
-                        sb.Append(",");
-                    }
-
-                    sb.Append("('");
-                    sb.Append(sqlDate);
-                    sb.Append("',");
-                    sb.Append(line.Key);
+                if (first)
+                {
+                    first = false;
+                }
+                else
+                {
                     sb.Append(",");
-                    sb.Append(Convert.ToDouble(line.Value).ToString(Tools.ciEnUS));
-                    sb.Append(")");
                 }
 
-
-
-                using (MySqlConnection con = new MySqlConnection(DBHelper.DBConnectionstring))
-                {
-                    con.Open();
-                    MySqlCommand cmd = new MySqlCommand(sb.ToString(), con);
-                    cmd.ExecuteNonQuery();
-
-                    try
-                    {
-                        string lastscanmyteslafilepaht = System.IO.Path.Combine(FileManager.GetExecutingPath(), "LASTSCANMYTESLA");
-                        System.IO.File.WriteAllText(lastscanmyteslafilepaht, sqlDate);
-                    }
-                    catch (Exception)
-                    { }
-
-                    return "insert ok ["+ kv.Keys.Count + "] "+  d.ToString();
-                }
+                sb.Append("('");
+                sb.Append(sqlDate);
+                sb.Append("',");
+                sb.Append(line.Key);
+                sb.Append(",");
+                sb.Append(Convert.ToDouble(line.Value).ToString(Tools.ciEnUS));
+                sb.Append(")");
             }
-            catch (Exception ex)
+
+            using (MySqlConnection con = new MySqlConnection(DBHelper.DBConnectionstring))
             {
-                Logfile.ExceptionWriter(ex, resultContent);
-            }
+                con.Open();
+                MySqlCommand cmd = new MySqlCommand(sb.ToString(), con);
+                cmd.ExecuteNonQuery();
 
-            return "NULL";
+                try
+                {
+                    string lastscanmyteslafilepaht = Path.Combine(FileManager.GetExecutingPath(), "LASTSCANMYTESLA");
+                    File.WriteAllText(lastscanmyteslafilepaht, sqlDate);
+                }
+                catch (Exception)
+                { }
+            }
+        }
+
+        private class Data
+        {
+            public Dictionary<string, object> Values;
+            public DateTime Timestamp;
         }
     }
 }
